bef21c8c8f402636da5add4da4f8bf907272f8fc
luan <aluncard6@gmail.com>
2022-02-28 22:05:58 -0300

    remover despesa

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ class Bd{
             if(despesa==null){
                 continue
             }
+            despesa.id=i
             despesas.push(despesa)
         }
         return despesas
@@ -97,6 +98,10 @@ class Bd{
         
 
 
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 
 }
@@ -216,6 +221,20 @@ function carregaListaDespesas(despesas = Array()) {
         linha.insertCell(1).innerHTML=`${d.tipo}`
         linha.insertCell(2).innerHTML=`${d.descricao}`
         linha.insertCell(3).innerHTML=`${d.valor}`
+
+        //criar botão de exclusão
+        let btn = document.createElement('button')
+        btn.className='btn btn-danger'
+        btn.innerHTML='<i class="fas fa-times"></i>'
+        btn.id=`id_despesa_${d.id}`
+        btn.onclick= function(){//remover a despesa
+            let id = this.id.replace('id_despesa_','')
+            bd.remover(id)
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
     })
 
 
